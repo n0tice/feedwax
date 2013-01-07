93da826624477d2405a6e52066e2bc54eb0cda43
Matt McAlister <mattmcal@yahoo.com>
2013-01-07 11:00:50 +0000

    generalizing cache headers

---
+++ config/cacheheader.php
@@ -0,0 +1,15 @@
+<?php
+$tsstring = gmdate('D, d M Y H:i:s ', $timestamp) . 'GMT';
+$etag = md5($language . $timestamp);
+
+$if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false;
+$if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
+if ((($if_none_match && $if_none_match == $etag) || (!$if_none_match)) && ($if_modified_since && $if_modified_since == $tsstring))
+{
+    header('HTTP/1.1 304 Not Modified');
+    exit();
+} else {
+    header("Last-Modified: $tsstring");
+    header("ETag: \"{$etag}\"");
+}
+?>
