98b3e6dddb7fed0235e311558e7299af60f186df
Matt McAlister <mattmcal@yahoo.com>
2013-05-08 13:08:55 +0100

    warning notice
    tell users that we are changing the way FeedWax works

---
+++ policy-change.php
@@ -0,0 +1,6 @@
+<?php
+echo "<p class=\"text-warning\"><b>PLEASE NOTE: </b> due to the high volume of content we are collecting on behalf of our users and the increasingly complicated use restrictions from the various social platforms we are adjusting the way these tools work.</p>
+<p class=\"text-warning\"><em>We've made it a bit too easy for you to collect content that might be restricted.</em></p>
+<p class=\"text-warning\">In order to protect you we will have to change our tools so that you can't inadvertently (or intentionally) break the terms & conditions or copyrights held by these platforms and the content owned by users there.</p>
+<p class=\"text-warning\">However, if you do in fact have the rights to the content you are collecting via FeedWax we can still help you.  Contact us at support@n0tice.com.</p><br>";
+?>
