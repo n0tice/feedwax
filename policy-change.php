ea97f0c3dc5eb8cb81e6b7f2a70dc8cee5f98bff
Matt McAlister <mattmcal@yahoo.com>
2013-05-08 17:33:39 +0100

    copy update

--- policy-change.php
+++ policy-change.php
@@ -1,6 +1,6 @@
 <?php
-echo "<p class=\"text-warning\"><b>PLEASE NOTE: </b> due to the high volume of content we are collecting on behalf of our users and the increasingly complicated use restrictions from the various social platforms we are adjusting the way these tools work.</p>
-<p class=\"text-warning\"><em>We've made it a bit too easy for you to collect content that might be restricted.</em></p>
+echo "<p class=\"text-warning\"><b>POLICY UPDATE (May 2013): </b> Due to the high volume of content we are collecting on behalf of our users and the increasingly complicated use restrictions from the various social platforms we are adjusting the way these tools work.</p>
+<blockquote><p class=\"text-warning\"><em>We've made it a bit too easy for you to collect content that might actually have use restrictions.</em></p></blockquote>
 <p class=\"text-warning\">In order to protect you we will have to change our tools so that you can't inadvertently (or intentionally) break the terms & conditions or copyrights held by these platforms and the content owned by users there.</p>
-<p class=\"text-warning\">However, if you do in fact have the rights to the content you are collecting via FeedWax we can still help you.  Contact us at support@n0tice.com.</p><br>";
+<p class=\"text-warning\">However, if you do in fact have the rights to the content you are collecting via FeedWax we can still help you.  Contact us at support@n0tice.com.  Thanks for your patience.</p><br>";
 ?>
